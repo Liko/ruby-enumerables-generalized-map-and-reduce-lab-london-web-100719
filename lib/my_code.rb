55b16213a2cc59f73a2ea1a68efb83e42a123ccf
Li Xie <li_xie1992@hotmail.com>
2019-09-09 22:29:34 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -21,27 +21,6 @@ def reduce(array, start = 0)
     new_value = yield(new_value, array[i])
     i += 1
   end 
- 
-  
-  array.length.times do |i|
-    new_value yield array[i]
-  end
-  
+
   new_value
 end 
-
-
-def reduce(s, sp=nil)
-  if sp
-    accum=sp
-    i=0
-  else
-    accum=s[0]
-    i=1
-  end
-  while i <s.length
-    accum = yield(accum, s[i])
-    i+=1
-  end
-  accum
-end
